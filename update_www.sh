94652ce6176cc78221b7767f61e68091c63522da
polytan02 <polytan02@mcgva.org>
2015-02-07 12:51:23 +0000

    Automatic update script

---
+++ update_www.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Setup of colours for error codes
+set -e;
+txtgrn=$(tput setaf 2);    # Green
+txtred=$(tput setaf 1);    # Red
+txtcyn=$(tput setaf 6);    # Cyan
+txtpur=$(tput setaf 5);    # Purple
+txtrst=$(tput sgr0);       # Text reset
+failed=[${txtred}FAILED${txtrst}];
+ok=[${txtgrn}OK${txtrst}];
+info=[${txtcyn}INFO${txtrst}];
+script=[${txtpur}SCRIPT${txtrst}];
+
+
+sources=/home/maxbilh/git/twitter-bootstrap-test/bootstrap;
+dest=/var/www/webapp_maxtest/site;
+
+echo -e "$ok Dossier sources : $sources";
+echo -e "$ok Dossier destination : $dest";
+
+echo -e "\n$script Copie des fichiers vers www/site \n";
+read -e -p "On continue ? ? (yn) : " -i "y" s1;
+if [ $s1 == 'y' ];
+        then sudo rm -rf $dest/*;
+        sudo cp -av $sources/* $dest/;
+        sudo chown -vR maxbilh:www-data $dest;
+        echo -e "\n$ok Copie terminée...";
+        else echo -e "\nOk, on ne fait rien\n";
+        read -e -p "Presser \" entrée \"...  ";
+fi;
+
